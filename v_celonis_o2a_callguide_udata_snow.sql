0891ce0f4e33ad08740defcc1ac7755a2c6cd53f
Amatya, Suyesh <suyesh.amatya@teliacompany.com>
2022-08-31 12:41:34 +0200

    new callguide views

---
+++ v_celonis_o2a_callguide_udata_snow.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE VIEW prod_swe_access.v_celonis_o2a_callguide_udata_snow AS (
+    SELECT
+        udata.ivTaskNumber
+        , udata.contact_id
+        , udata.firstContactId
+        , udata.firstContactSourceType
+        , udata.origContactId
+        , udata.origContactSourceType
+        , udata.Menuchoice
+        , udata.Inittarget
+        , udata.Target
+        , CAST(udata.routeReqTime AS TIMESTAMP) AS routeReqTime
+        , udata.ivInteractionTimeStart
+
+        FROM (
+        SELECT
+
+              id.contact_id
+            , id.udata_value AS ivTaskNumber
+            , MAX(CASE WHEN udata.udata_key = 'firstContactId'          THEN udata.udata_value END) AS firstContactId
+            , MAX(CASE WHEN udata.udata_key = 'origContactId'           THEN udata.udata_value END) AS origContactId
+            , MAX(CASE WHEN udata.udata_key = 'firstContactSourceType'  THEN udata.udata_value END) AS firstContactSourceType
+            , MAX(CASE WHEN udata.udata_key = 'origContactSourceType'   THEN udata.udata_value END) AS origContactSourceType
+            , MAX(CASE WHEN udata.udata_key = 'Menuchoice'              THEN udata.udata_value END) AS Menuchoice
+            , MAX(CASE WHEN udata.udata_key = 'Inittarget'              THEN udata.udata_value END) AS Inittarget
+            , MAX(CASE WHEN udata.udata_key = 'Target'                  THEN udata.udata_value END) AS Target
+            , MAX(CASE WHEN udata.udata_key = 'routeReqTime'            THEN udata.udata_value END) AS routeReqTime
+            , MAX(CASE WHEN udata.udata_key = 'ivInteractionTimeStart'  THEN udata.udata_value END) AS ivInteractionTimeStart
+            , MAX(CASE WHEN udata.udata_key = 'pnr'                     THEN udata.udata_value END) AS pnr
+
+        FROM prod_swe_access.t_callguide_cg_hist_udata_gdpr AS id
+
+        INNER JOIN prod_swe_access.t_callguide_cg_hist_udata_gdpr AS udata ON udata.contact_id = id.contact_id
+
+        WHERE id.orgarea_name = 'pri'
+            AND id.udata_key = 'ivTaskNumber'
+            AND id.udata_value IS NOT NULL
+            AND id.dt_last_changed_time > '2022-06-05'
+
+        GROUP BY
+            id.contact_id
+            , id.udata_value
+
+        ) AS udata
+
+        INNER JOIN prod_swe_access.v_celonis_snow_case AS sb ON sb.case_number = udata.ivTaskNumber
+
+        ORDER BY udata.contact_id
+
+);
